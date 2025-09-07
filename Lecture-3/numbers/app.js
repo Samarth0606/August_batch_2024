4cf804017826f484e8339021cf1e1c662d3d3bce
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-09-07 21:22:29 +0530

    3rd commit aug batch codes

---
+++ Lecture-3/numbers/app.js
@@ -0,0 +1,20 @@
+
+
+let sam = 10 
+let mav = 10.50; 
+let vohra = -10.99
+
+// printing statement
+console.log(sam);
+
+console.log(mav)
+
+console.log(vohra)
+
+console.log( typeof(sam) )   //number
+ 
+console.log( typeof(mav) )   //number
+
+console.log( typeof(vohra) ) //number
+
+
---
+++ Lecture-3/string/app.js
@@ -0,0 +1,16 @@
+
+let a  = 'sam';
+let b  = "sam";
+let c  = `sam`; //backticks
+// let d  = new String('sam'); //worst way
+
+console.log(a);
+console.log(b);
+console.log(c);
+// console.log(d);
+
+console.log(a , b , c);
+
+
+
+
