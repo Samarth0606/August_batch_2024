72f9904bb0ed95a640b79e6a8e0883c177de2a0e
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-09-15 20:58:38 +0530

    7th commit aug batch codes

---
+++ Lecture-7/h/app.js
@@ -0,0 +1,26 @@
+
+
+// var a = 10;
+// console.log(a); // 10
+
+// function sam(){
+//     console.log("sam");
+// }
+// sam(); // sam
+
+
+// ----------------------------
+
+
+console.log(a)
+var a = 100;
+console.log(a)
+
+function vohra(){
+    console.log(c);
+    var c = "lol";
+    console.log(c);
+}
+vohra()
+
+console.log(a);
---
+++ Lecture-7/var_let_const/app.js
@@ -0,0 +1,46 @@
+// var a = 10;   // declaration
+// console.log(a); // 10
+
+// var a = true; // redeclaration ✅
+// console.log(a); // true
+
+// --------------------------------
+
+// var a = 10;   // declaration
+// console.log(a); // 10
+
+// a = true; // reassign ✅
+// console.log(a); // true
+
+
+// ----------------------------------------------------------------
+
+// let a = 10;
+// console.log(a);
+
+// a = true; // reassign ✅
+// console.log(a);
+
+// --------------------------------
+
+// let x = 1;
+// console.log(x);
+// let x = true; // redeclare ❌
+// console.log(x);
+
+// ----------------------------------------------------------------
+
+// const a = 10
+// console.log(a);
+
+// a = 100; // reassign ❌
+// console.log(a);
+
+// --------------------------------
+
+// const x = 10
+// console.log(x);
+
+// const x = 500; // redeclarare ❌
+// console.log(x);
+
