9d814a32997ca9a9e06aa911f5e96c0c552a2b79
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-09-19 19:18:35 +0530

    8th commit aug batch codes

---
+++ Lecture-8/hoisting/app.js
@@ -0,0 +1,19 @@
+
+
+console.log(a);
+
+var a = 10;
+
+console.log(a);
+
+sam()
+
+function sam(){
+    console.log(b);
+    var b = "sam"
+    console.log(b);
+}
+
+console.log(a);
+
+sam()
---
+++ Lecture-8/temporal_dead_zone/app.js
@@ -0,0 +1,41 @@
+
+// console.log(x); //undefined
+// var x = 100;
+
+// -----------------------------
+
+// console.log(y); //
+// let y = 100;
+
+// -----------------------------
+
+
+// console.log(a);    // GENERAL HOISTING
+// console.log(b);    // DTZ (dead temporal zone)
+// console.log(c);
+
+// var a = 10;
+// let b = 100;
+// const c = 1000;
+
+// -----------------------------
+
+
+console.log(a);
+console.log(b);
+var a = 1;
+let b = 2;
+let c = 3;
+console.log(a);
+console.log(b);
+console.log(c);
+
+function boy(){
+    console.log("i am boy");
+}
+boy();
+
+
+
+
+
