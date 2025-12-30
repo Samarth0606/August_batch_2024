b49d9ee3e132b2f9121ad3a2eb500dbaa72a3b94
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-12-30 20:50:58 +0530

    10th react commit aug batch codes

--- React-10/react_10/src/App.jsx
+++ React-10/react_10/src/App.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Reff from './components/Reff'
 import Refff from './components/Refff'
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+// import Dashboard from './components/Dashboard'
+const Dashboard = React.lazy( ()=> import('./components/Dashboard') )
+
+// import Profile from './components/Profile'
+const Profile = React.lazy( ()=>import('./components/Profile') )
+
 
 function App() {
   return (
-    <div>
-      {/* <Reff /> */}
-      <Refff />
-    </div>
+    <>
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='/dashboard' element={ <Suspense fallback="loading1..."> <Dashboard/> </Suspense> } />
+          <Route path='/profile' element={ <Suspense fallback="loading2..."> <Profile/> </Suspense> } />
+        </Routes>
+        <div>
+          <Link to='/dashboard'>Dashboard</Link>
+          <Link to='/profile'>Profile</Link>
+          {/* <Reff /> */}
+          {/* <Refff /> */}
+        </div>
+    </>
   )
 }
 
