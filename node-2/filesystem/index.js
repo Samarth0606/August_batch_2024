2f4bc94bb6c27d5a6b21b8839995f635963e7e1b
Samarth Vohra <officialdevelopersamarth@gmail.com>
2026-01-13 20:00:16 +0530

    node 2 commit aug batch codes

--- node-2/filesystem/index.js
+++ node-2/filesystem/index.js
@@ -4,16 +4,16 @@ const fs = require('fs'); // commonjs
 //CREATE
 // fs.writeFileSync('sam.txt', "hey, welcome to @code_baithak")
 
-// fs.writeFile('mav.txt', "Welcome Mav", {flag:'w', encoding:'utf-8'}, (err,result)=>{
-//     if(err) throw err;
-//     else{
-//         console.log("file written successfully");  
-//     }
-// })
+fs.writeFile('mav.txt', "Welcome Mav", {flag:'w', encoding:'utf-8'}, (err,result)=>{
+    if(err) throw err;
+    else{
+        console.log("file written successfully");  
+    }
+})
 
 // READ
 // const result = fs.readFileSync('sam.txt', 'utf-8')
-// const result = fs.readFileSync('sam.txt', {encoding: 'utf-8'})
+const result = fs.readFileSync('sam.txt', {flag:'r', encoding: 'utf-8'})
 
 // 2nd way
 // const result = fs.readFileSync('sam.txt')
