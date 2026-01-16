461dc1c53a6ceb555590241f9035c9202655bb93
Samarth Vohra <officialdevelopersamarth@gmail.com>
2026-01-16 18:52:12 +0530

    3rd node commit aug batch codes

--- node-3/express/index.js
+++ node-3/express/index.js
@@ -8,6 +8,14 @@ app.get( '/'      , (req,res)=>{ res.send("welcome to root route") } );
 // app.get( '/home'  , (req,res)=>{} );
 // app.get( '/about' , (req,res)=>{} );
 
+app.get('/sam' , (req,res)=>{
+    res.send("hey i am get method of sam")
+})
+
+app.post('/sam' , (req,res)=>{
+    res.send("hey i am post method of sam")
+})
+
 
 const PORT = 8080;
 app.listen(PORT , ()=>{
