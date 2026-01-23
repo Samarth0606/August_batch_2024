5c91aad6e38785451cea9a576e20d00466c4b8f5
Samarth Vohra <officialdevelopersamarth@gmail.com>
2026-01-23 20:38:58 +0530

    6th node commit aug batch codes

--- node-6/swiggy_backend_02/controllers/user.controller.js
+++ node-6/swiggy_backend_02/controllers/user.controller.js
@@ -1,7 +1,21 @@
+import UserModel from "../models/User.model.js";
+import bcrypt from 'bcrypt';
 // register
 export async function register(req,res){
     try{
-       
+       let {fullName,email,password} = req.body;
+       const data = await UserModel.findOne({email});
+       if(data){
+        return res.status(409).json({message: "user already exists"})
+       }
+       else{
+        const newUser = await UserModel.create({
+            fullName,
+            email,
+            password: bcrypt.hashSync(password, 10)
+        })
+        return res.status(201).json({message:newUser})
+       }
     }
     catch(error){
         return res.status(500).json({"err": error})
@@ -12,7 +26,25 @@ export async function register(req,res){
 // login
 export async function login(req,res){
     try{
-        
+       let {email,password} = req.body;
+       const data = await UserModel.findOne({email});
+       if(!data){
+        return res.status(409).json({message: "user doesnot exists"})
+       }
+       else{
+        let validPassword = bcrypt.compareSync(password,data.password)
+        if(!validPassword){
+            return res.status(403).json({message: "wrong credentials"})
+        }
+        return res.status(200).json({
+            user: {
+                email: data.email,
+                fullName: data.fullName,
+                // password: data.password
+            },
+            // accessToken: JWT TOKEN
+        })
+       }
     }
     catch(error){
         return res.status(500).json({"err": error})
