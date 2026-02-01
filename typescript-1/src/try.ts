b80cec5dc658834ec748f979a3a748a072ac9215
Samarth Vohra <officialdevelopersamarth@gmail.com>
2026-02-01 13:07:32 +0530

    typescript day 1

---
+++ typescript-1/src/try.ts
@@ -0,0 +1,69 @@
+// let a = "Samarth Vohra";
+// console.log(a);
+// --------------------------
+// data security
+// let num = 10;
+// // num = "sam";
+
+// num = 400
+// num =  -100
+// // num = true
+// num = "string"
+
+// --------------------------
+
+// 1. write a fn that greets with your firstname
+
+// argument = firstname
+// print - hello sam
+// function which doesnot returns anything
+
+// function greet(firstName: String){
+//     console.log(`hello ${firstName}`);
+// }
+// greet("Sam")
+
+// 2. write a fn that calculates sum of 2 numbers
+
+// function sum(a:number,b:number){
+//     console.log(a+b);
+// }
+// sum(10,23)
+
+// 3. write a fn that checks age(args) age>18 = true else false
+// function ageCheck(age: number):boolean{
+//     return age > 18 ? true:false
+//     // return 10
+// }
+// ageCheck(19)
+
+// 4. write a fn that accepts another fn and executes it after 1s
+
+// function a(){
+//     console.log("hi hello");
+//     return 10
+// }
+
+// function b(cb: ()=>number){
+//     setTimeout(()=>{
+//         cb();
+//     }, 1000)
+// }
+
+// b(a)
+
+// --------------------------------
+
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// function sam(){ console.log("hello") }
+
+// let vohra = ()=>{ console.log("vohra") }
+
+
+function sam(a){
+    console.log(a);
+}
+sam(100)
